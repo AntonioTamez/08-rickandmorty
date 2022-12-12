9c55fcc43986ffaed0c03ba5a75b1a9726f1a020
Antonio <Antonio.Tamez@chubb.com>
2022-12-12 16:39:43 -0600

    se agrega splash e icono

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.rickandmorty',
+  appId: 'com.antoniotamez.rickandmorty',
   appName: 'rickandmorty',
   webDir: 'www',
   bundledWebRuntime: false
